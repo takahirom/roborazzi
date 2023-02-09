082e9fa0644fa69d7db5fb728143c629fe7cc7f7
takahirom <takam.dev@gmail.com>
2023-02-09 12:27:24 +0900

    Add benchmark

---
+++ app/src/test/java/com/github/takahirom/roborazzi/sample/RuleTestWithoutRoborazziForBenchmark.kt
@@ -0,0 +1,28 @@
+package com.github.takahirom.roborazzi.sample
+
+import androidx.test.core.app.ActivityScenario.launch
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.Espresso.pressBack
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Assert.*
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class RuleTestWithoutRoborazziForBenchmark {
+  @Test
+  fun captureRoboGifSample() {
+    // launch
+    launch(MainActivity::class.java)
+    // move to next page
+    onView(withId(R.id.button_first))
+      .perform(click())
+    // back
+    pressBack()
+    // move to next page
+    onView(withId(R.id.button_first))
+      .perform(click())
+  }
+}
