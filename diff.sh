e626561cfdf3fdd72123fc530c815ba566c43596
takahirom <takam.dev@gmail.com>
2023-02-18 18:50:43 +0900

    Change script

--- diff.sh
+++ diff.sh
@@ -1,7 +1,9 @@
 curl -L https://nightly.link/takahirom/roborazzi/workflows/test.yaml/main/screenshots.zip > original.zip
 unzip -o original.zip -d original
-./gradlew app:testDebugUnitTest --stacktrace
+rm -R app/build/outputs/roborazzi
+./gradlew app:testDebugUnitTest --stacktrace --rerun
 cd app/build/outputs/roborazzi
 mkdir diff
 find . -name "*.png" -or -name "*.gif" | xargs -IIMG compare ../../../../original/IMG IMG diff/IMG
+open diff
 
