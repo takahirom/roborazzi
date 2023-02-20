0158210ec54ff5eb56706d3f18088f02fe92f9b7
takahirom <takam.dev@gmail.com>
2023-02-20 10:58:23 +0900

    Remove unneeded

--- roborazzi-painter/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.github.takahirom.roborazzi {
-    requires java.base;
-    requires java.desktop;
-    requires java.xml;
-    requires kotlin.stdlib;
-    // comes from Gradle
-//    requires android.all.Q.robolectric;
-}
