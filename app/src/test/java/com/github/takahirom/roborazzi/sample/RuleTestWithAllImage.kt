93b16f630fb21d89aa616b34ffff6a0ce0806258
takahirom <takam.dev@gmail.com>
2023-03-27 10:08:52 +0900

    Add test

--- app/src/test/java/com/github/takahirom/roborazzi/sample/RuleTestWithAllImage.kt
+++ app/src/test/java/com/github/takahirom/roborazzi/sample/RuleTestWithAllImage.kt
@@ -1,8 +1,14 @@
 package com.github.takahirom.roborazzi.sample
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.view.View
+import android.widget.ImageView
 import androidx.test.core.app.ActivityScenario.launch
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.Espresso.pressBack
+import androidx.test.espresso.UiController
+import androidx.test.espresso.ViewAction
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.matcher.ViewMatchers.isRoot
 import androidx.test.espresso.matcher.ViewMatchers.withId
@@ -26,6 +32,19 @@ class RuleTestWithAllImage {
   fun captureRoboGifSample() {
     // launch
     launch(MainActivity::class.java)
+
+    // Change image
+    onView(withId(R.id.image))
+      .perform(object : ViewAction {
+        override fun getDescription() = ""
+
+        override fun getConstraints() = withId(R.id.image)
+
+        override fun perform(uiController: UiController?, view: View?) {
+          (view as ImageView).setImageDrawable(ColorDrawable(Color.RED))
+        }
+      })
+
     // move to next page
     onView(withId(R.id.button_first))
       .perform(click())
