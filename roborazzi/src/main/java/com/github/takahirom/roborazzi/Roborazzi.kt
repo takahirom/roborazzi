f38b7baaac395030076265883f4c9cc7f792cc09
takahirom <takam.dev@gmail.com>
2023-04-21 11:03:17 +0900

    Once quit experimental

--- roborazzi/src/main/java/com/github/takahirom/roborazzi/Roborazzi.kt
+++ roborazzi/src/main/java/com/github/takahirom/roborazzi/Roborazzi.kt
@@ -24,11 +24,6 @@ import org.hamcrest.Matchers
 
 
 const val DEFAULT_ROBORAZZI_OUTPUT_DIR_PATH = "build/outputs/roborazzi"
-
-@RequiresOptIn("This Roborazzi API is experimental and is likely to be changed or removed entirely")
-annotation class RoborazziExperimetalApi
-
-@RoborazziExperimetalApi
 var ROBORAZZI_DEBUG = false
 fun roborazziEnabled(): Boolean {
   val isEnabled = roborazziRecordingEnabled() ||
@@ -43,17 +38,14 @@ fun roborazziEnabled(): Boolean {
   return isEnabled
 }
 
-@RoborazziExperimetalApi
 fun roborazziCompareEnabled(): Boolean {
   return System.getProperty("roborazzi.test.compare") == "true"
 }
 
-@RoborazziExperimetalApi
 fun roborazziVerifyEnabled(): Boolean {
   return System.getProperty("roborazzi.test.verify") == "true"
 }
 
-@RoborazziExperimetalApi
 fun roborazziRecordingEnabled(): Boolean {
   return System.getProperty("roborazzi.test.record") == "true"
 }
@@ -491,7 +483,6 @@ private fun log(message: String) {
   println("Roborazzi: $message")
 }
 
-@OptIn(RoborazziExperimetalApi::class)
 private inline fun debugLog(crossinline message: () -> String) {
   if (ROBORAZZI_DEBUG) {
     println("Roborazzi Debug: ${message()}")
