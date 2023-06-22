a0e2da22bfd97f03eaa93a54ff02e10a3fd82a26
takahirom <takam.dev@gmail.com>
2023-06-22 10:08:21 +0900

    RoborazziOption can now be set in RoborazziRule and used in captureRoboImage()

--- roborazzi/src/main/java/com/github/takahirom/roborazzi/RoborazziContext.kt
+++ roborazzi/src/main/java/com/github/takahirom/roborazzi/RoborazziContext.kt
@@ -4,6 +4,8 @@ package com.github.takahirom.roborazzi
 object RoborazziContext {
   private var runnerOverrideOutputDirectory: String? = null
   private var ruleOverrideOutputDirectory: String? = null
+  private var runnerOverrideRoborazziOptions: RoborazziOptions? = null
+  private var ruleOverrideRoborazziOptions: RoborazziOptions? = null
 
   fun setRunnerOverrideOutputDirectory(outputDirectory: String) {
     runnerOverrideOutputDirectory = outputDirectory
@@ -21,12 +23,35 @@ object RoborazziContext {
     ruleOverrideOutputDirectory = null
   }
 
+  fun setRunnerOverrideRoborazziOptions(options: RoborazziOptions) {
+    runnerOverrideRoborazziOptions = options
+  }
+
+  fun clearRunnerOverrideRoborazziOptions() {
+    runnerOverrideRoborazziOptions = null
+  }
+
+  fun setRuleOverrideRoborazziOptions(options: RoborazziOptions) {
+    ruleOverrideRoborazziOptions = options
+  }
+
+  fun clearRuleOverrideRoborazziOptions() {
+    ruleOverrideRoborazziOptions = null
+  }
+
   val outputDirectory
     get() = if (ruleOverrideOutputDirectory != null) {
       ruleOverrideOutputDirectory
     } else {
       runnerOverrideOutputDirectory
     } ?: DEFAULT_ROBORAZZI_OUTPUT_DIR_PATH
+
+  val options
+    get() = if (ruleOverrideRoborazziOptions != null) {
+      ruleOverrideRoborazziOptions
+    } else {
+      runnerOverrideRoborazziOptions
+    } ?: RoborazziOptions()
 }
 
 @InternalRoborazziApi
