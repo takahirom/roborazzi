50423459abff911641bb448f02ffdd466b708b81
takahirom <takam.dev@gmail.com>
2023-07-06 23:58:30 +0900

    Remove integration test from this PR

--- include-build/roborazzi-gradle-plugin/src/integrationTest/projects/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
-plugins {
-  id("com.android.application") apply false
-  id("org.jetbrains.kotlin.android") apply false
-  // Just for Gradle Build, included build will be applied
-  id("io.github.takahirom.roborazzi") version "1.0.0" apply false
-}
-true // Needed to make the Suppress annotation work for the plugins block
