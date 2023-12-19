948866fe87f975f8567d9f1fbb3b89b020663ee7
Kazunori Jo <519604+itochan@users.noreply.github.com>
2023-12-20 00:51:30 +0900

    Migrate to androidx.activity:actiity to version catalog

--- include-build/roborazzi-gradle-plugin/src/integrationTest/java/io/github/takahirom/roborazzi/RoborazziGradleProject.kt
+++ include-build/roborazzi-gradle-plugin/src/integrationTest/java/io/github/takahirom/roborazzi/RoborazziGradleProject.kt
@@ -232,7 +232,7 @@ dependencies {
 
   implementation(libs.androidx.core.ktx)
   implementation(libs.androidx.lifecycle.runtime.ktx)
-  implementation("androidx.activity:activity:1.7.2")
+  implementation(libs.androidx.activity)
 //  implementation(libs.androidx.activity.compose)
   implementation(platform(libs.androidx.compose.bom))
 //  implementation(libs.androidx.compose.ui)
