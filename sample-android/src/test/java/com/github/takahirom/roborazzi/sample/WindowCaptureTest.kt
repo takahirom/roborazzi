5c492018028e5ea48a1a046c72783dfc4ac6558b
takahirom <takam.dev@gmail.com>
2024-01-03 13:29:49 +0900

    Add compose modal dialog test

--- sample-android/src/test/java/com/github/takahirom/roborazzi/sample/WindowCaptureTest.kt
+++ sample-android/src/test/java/com/github/takahirom/roborazzi/sample/WindowCaptureTest.kt
@@ -7,17 +7,37 @@ import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.AlertDialog
+import androidx.compose.material.Button
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Icon
+import androidx.compose.material.ListItem
+import androidx.compose.material.ModalBottomSheetLayout
+import androidx.compose.material.ModalBottomSheetValue
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.rememberModalBottomSheetState
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.unit.dp
 import androidx.test.core.app.ActivityScenario
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.github.takahirom.roborazzi.*
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import kotlinx.coroutines.launch
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -38,7 +58,7 @@ class WindowCaptureTest {
   fun composeDialog() {
     composeTestRule.setContent {
       Column(
-        modifier = androidx.compose.ui.Modifier
+        modifier = Modifier
           .background(Color.Cyan)
           .fillMaxSize()
       ) {
@@ -60,11 +80,66 @@ class WindowCaptureTest {
     captureScreenRoboImage()
   }
 
+
+  @OptIn(ExperimentalMaterialApi::class)
+  @Test
+  fun composeBottomSheet() {
+    composeTestRule.setContent {
+      Column(
+        modifier = Modifier
+          .background(Color.Cyan)
+          .fillMaxSize()
+      ) {
+        Text("Under the dialog")
+        // FROM: https://foso.github.io/Jetpack-Compose-Playground/material/modalbottomsheetlayout/
+        val state = rememberModalBottomSheetState(ModalBottomSheetValue.Hidden)
+        val scope = rememberCoroutineScope()
+        ModalBottomSheetLayout(
+          {
+            LazyColumn {
+              items(50) {
+                ListItem(
+                  text = { Text("Item $it") },
+                  icon = {
+                    Icon(
+                      Icons.Default.Favorite,
+                      contentDescription = "Localized description"
+                    )
+                  }
+                )
+              }
+            }
+          }, sheetState = state
+        ) {
+          Column(
+            modifier = Modifier
+              .fillMaxSize()
+              .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+          ) {
+            Text("Rest of the UI")
+            Spacer(Modifier.height(20.dp))
+            Button(onClick = { scope.launch { state.show() } }) {
+              Text("Click to show sheet")
+            }
+          }
+        }
+      }
+    }
+
+    composeTestRule
+      .onNode(hasText("Click to show sheet"))
+      .performClick()
+    composeTestRule.waitForIdle()
+
+    captureScreenRoboImage()
+  }
+
   @Test
   fun androidDialog() {
     composeTestRule.setContent {
       Column(
-        modifier = androidx.compose.ui.Modifier
+        modifier = Modifier
           .background(Color.Cyan)
           .fillMaxSize()
       ) {
@@ -88,7 +163,7 @@ class WindowCaptureTest {
   fun noDialog() {
     composeTestRule.setContent {
       Column(
-        modifier = androidx.compose.ui.Modifier
+        modifier = Modifier
           .background(Color.Cyan)
           .fillMaxSize()
       ) {
@@ -103,7 +178,7 @@ class WindowCaptureTest {
   fun dump() {
     composeTestRule.setContent {
       Column(
-        modifier = androidx.compose.ui.Modifier
+        modifier = Modifier
           .background(Color.Cyan)
           .fillMaxSize()
       ) {
@@ -131,9 +206,11 @@ class WindowCaptureTest {
       }
     }
 
-    captureScreenRoboImage(roborazziOptions = RoborazziOptions(
-      captureType = RoborazziOptions.CaptureType.Dump(),
-    ))
+    captureScreenRoboImage(
+      roborazziOptions = RoborazziOptions(
+        captureType = RoborazziOptions.CaptureType.Dump(),
+      )
+    )
   }
 }
 
@@ -155,6 +232,7 @@ class FragmentActivityWindowCaptureTest {
       })
     }
   }
+
   @Test
   fun bottomSheetDialog() {
     ROBORAZZI_DEBUG = true
