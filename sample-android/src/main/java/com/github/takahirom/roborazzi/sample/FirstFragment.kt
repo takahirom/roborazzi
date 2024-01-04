e8068cca1bc83ca359a94c312fa5098413e878c3
takahirom <takam.dev@gmail.com>
2024-01-04 17:31:55 +0900

    Add boarder to sample to debug

--- sample-android/src/main/java/com/github/takahirom/roborazzi/sample/FirstFragment.kt
+++ sample-android/src/main/java/com/github/takahirom/roborazzi/sample/FirstFragment.kt
@@ -5,12 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.testTag
@@ -76,6 +82,7 @@ fun SampleComposableFunction() {
       text = "Sub",
       Modifier
         .testTag("SubBoxButton")
+        .border(1.dp, Color.Black, MaterialTheme.shapes.medium)
         .background(Color.Gray)
         .clickable {
           count--
