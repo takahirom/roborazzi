f19f1a25d65c0c341a0a4c375e7e9f0b5eae05e9
takahirom <takam.dev@gmail.com>
2024-01-04 18:11:40 +0900

    Just change tests for checking

--- sample-android/src/main/java/com/github/takahirom/roborazzi/sample/FirstFragment.kt
+++ sample-android/src/main/java/com/github/takahirom/roborazzi/sample/FirstFragment.kt
@@ -5,12 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.testTag
@@ -76,7 +82,7 @@ fun SampleComposableFunction() {
       text = "Sub",
       Modifier
         .testTag("SubBoxButton")
-        .background(Color.Gray)
+        .background(Color.Gray, MaterialTheme.shapes.medium)
         .clickable {
           count--
         }
@@ -84,6 +90,7 @@ fun SampleComposableFunction() {
     (0 until count).forEach { index ->
       Box(
         Modifier
+          .border(1.dp, Color.Gray, MaterialTheme.shapes.small)
           .background(Color.Red)
           .testTag("child:$index")
           .size(32.dp)
