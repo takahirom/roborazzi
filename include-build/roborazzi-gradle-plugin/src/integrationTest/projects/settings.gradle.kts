d15be176b932810e550f765d6caa012cf84c010c
takahirom <takam.dev@gmail.com>
2024-04-16 11:23:25 +0900

    Use PREFER_PROJECT

--- include-build/roborazzi-gradle-plugin/src/integrationTest/projects/settings.gradle.kts
+++ include-build/roborazzi-gradle-plugin/src/integrationTest/projects/settings.gradle.kts
@@ -12,7 +12,7 @@ dependencyResolutionManagement {
       from(files("$roborazziRootPath/gradle/libs.versions.toml"))
     }
   }
-  repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+  repositoriesMode.set(RepositoriesMode.PREFER_PROJECT)
   repositories {
     google()
     mavenCentral()
