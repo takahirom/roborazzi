2ad5c40e1ab55136c1d02c7d67525d62669ac435
Takahiro Menju <takam.dev@gmail.com>
2024-04-18 22:31:42 +0900

    Update IosTest.kt

--- roborazzi-compose-ios/src/iosTest/kotlin/io/github/takahirom/roborazzi/IosTest.kt
+++ roborazzi-compose-ios/src/iosTest/kotlin/io/github/takahirom/roborazzi/IosTest.kt
@@ -31,7 +31,7 @@ class IosTest {
                 Button(
                   modifier = Modifier.alpha(alpha),
                   onClick = { /*TODO*/ }) {
-                  Text("Hello World6")
+                  Text("Hello iOS")
                 }
                 Box(
                   modifier = Modifier
