0d763a021c376532169e383abfbdda9980bf1390
takahirom <takam.dev@gmail.com>
2024-06-26 12:14:53 +0900

    Implement autoPreviewScreenshots

---
+++ sample-auto-preview/src/main/java/com/github/takahirom/sample/Preview.kt
@@ -0,0 +1,14 @@
+package com.github.takahirom.sample
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+fun Preview() {
+  Box {
+    Text("Hello, World!")
+  }
+}
