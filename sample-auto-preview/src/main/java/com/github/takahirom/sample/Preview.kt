5b04f57ae6bc1d4b16ab4e7181577ba4e4eb9192
takahirom <takam.dev@gmail.com>
2024-06-27 10:59:27 +0900

    Use AndroidPreviewScreenshotIdBuilder

--- sample-auto-preview/src/main/java/com/github/takahirom/sample/Preview.kt
+++ sample-auto-preview/src/main/java/com/github/takahirom/sample/Preview.kt
@@ -1,14 +1,59 @@
 package com.github.takahirom.sample
 
-import androidx.compose.foundation.layout.Box
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.Wallpapers
+import androidx.compose.ui.unit.dp
 
 @Preview
 @Composable
 fun Preview() {
-  Box {
-    Text("Hello, World!")
+  Card(
+      Modifier
+          .width(100.dp)
+          .height(50.dp)
+  ) {
+    Text(
+      modifier = Modifier.padding(8.dp),
+      text = "Hello, World!"
+    )
+  }
+}
+
+@Preview(
+  name = "Preview Name",
+  // These properties are not supported by Roborazzi yet.
+  group = "Preview Group",
+  apiLevel = 30,
+  widthDp = 320,
+  heightDp = 640,
+  locale = "ja_JP",
+  fontScale = 1.5f,
+  showSystemUi = true,
+  showBackground = true,
+  backgroundColor = 0xFF0000FF,
+  uiMode = Configuration.UI_MODE_NIGHT_YES,
+  device = Devices.NEXUS_5,
+  wallpaper = Wallpapers.GREEN_DOMINATED_EXAMPLE,
+)
+@Composable
+fun PreviewWithProperties() {
+  Card(
+      Modifier
+          .width(100.dp)
+          .height(50.dp)
+  ) {
+    Text(
+      modifier = Modifier.padding(8.dp),
+      text = "Hello, World!"
+    )
   }
 }
