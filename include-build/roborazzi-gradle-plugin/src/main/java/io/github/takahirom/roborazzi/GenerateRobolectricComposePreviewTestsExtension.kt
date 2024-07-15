f1f3c29154378b602b5c49d87259ea2e2fbc83f1
takahirom <takam.dev@gmail.com>
2024-07-15 14:56:38 +0900

    Fix an issue where warning logs are printed multiple times

--- include-build/roborazzi-gradle-plugin/src/main/java/io/github/takahirom/roborazzi/GenerateRobolectricComposePreviewTestsExtension.kt
+++ include-build/roborazzi-gradle-plugin/src/main/java/io/github/takahirom/roborazzi/GenerateRobolectricComposePreviewTestsExtension.kt
@@ -54,7 +54,6 @@ fun generateRobolectricPreviewTestsIfNeeded(
   project: Project,
   variant: Variant,
   extension: GenerateComposePreviewRobolectricTestsExtension,
-  androidExtension: TestedExtension,
   testTaskProvider: TaskCollection<Test>
 ) {
   if ((extension.enable.orNull) != true) {
@@ -73,9 +72,7 @@ fun generateRobolectricPreviewTestsIfNeeded(
     check(variant.unitTest != null) {
       "Roborazzi: Please enable unit tests for the variant '${variant.name}' in the 'build.gradle' file."
     }
-    verifyMavenRepository(project)
-    verifyLibraryDependencies(project)
-    verifyTestConfig(testTaskProvider, androidExtension, logger)
+    verifyTestConfig(testTaskProvider, logger)
   }
 }
 
@@ -108,126 +105,6 @@ private fun setupGeneratePreviewTestsTask(
     generateTestsTask,
     GeneratePreviewScreenshotTestsTask::outputDir
   )
-}
-
-private fun verifyMavenRepository(project: Project) {
-  // Check if the jitpack repository is added.
-  val hasJitpackRepo = project.repositories.any {
-    it is MavenArtifactRepository &&
-      it.url.toString().contains("https://jitpack.io")
-  }
-  // settingsEvaluated{} is not called
-  // when it is already evaluated so we need to call `settingsEvaluated`
-  // before the project is evaluated.
-  // So we need to use the following workaround.
-  // https://github.com/gradle/gradle/issues/27741
-  val setting = (project.gradle as? DefaultGradle)?.getSettings() ?: return
-  val hasJitpackRepoInSettings = setting.pluginManagement.repositories.any {
-    it is MavenArtifactRepository && it.url.toString().contains("https://jitpack.io")
-  } || setting.dependencyResolutionManagement.repositories.any {
-    it is MavenArtifactRepository && it.url.toString().contains("https://jitpack.io")
-  }
-  if (!hasJitpackRepo && !hasJitpackRepoInSettings) {
-    error(
-      "Roborazzi: Please add the following 'maven' repository to the 'repositories' block in the 'build.gradle' file.\n" +
-        "build.gradle: \nrepositories {\nmaven { url 'https://jitpack.io' } \n}\n" +
-        "build.gradle.kts: \nrepositories {\nmaven { url = uri(\"https://jitpack.io\") } \n}\n" +
-        "This is necessary to download the ComposablePreviewScanner."
-    )
-  }
-}
-
-private fun verifyTestConfig(
-  testTaskProvider: TaskCollection<Test>,
-  androidExtension: TestedExtension,
-  logger: Logger
-) {
-  if (!androidExtension.testOptions.unitTests.isIncludeAndroidResources) {
-    logger.warn(
-      "Roborazzi: Please set 'android.testOptions.unitTests.isIncludeAndroidResources = true' in the 'build.gradle' file. " +
-        "This is advisable to avoid issues with ActivityNotFoundException."
-    )
-  }
-  testTaskProvider.configureEach { testTask ->
-    if (testTask.systemProperties["roborazzi.record.filePathStrategy"] != "relativePathFromRoborazziContextOutputDirectory") {
-      logger.info(
-        "Roborazzi: Please set 'roborazzi.record.filePathStrategy=relativePathFromRoborazziContextOutputDirectory' in the 'gradle.properties' file. " +
-          "This is advisable to avoid unnecessary path manipulation. " +
-          "Please refer to 'https://github.com/takahirom/roborazzi?tab=readme-ov-file#roborazzirecordfilepathstrategy' for more information."
-      )
-    }
-    if (testTask.systemProperties["robolectric.pixelCopyRenderMode"] != "hardware") {
-      val example = """
-        android {
-          testOptions {
-            unitTests {
-              isIncludeAndroidResources = true
-              all {
-                it.systemProperties["robolectric.pixelCopyRenderMode"] = "hardware"
-              }
-            }
-          }
-        }
-      """.trimIndent()
-      logger.warn(
-        "Roborazzi: Please set 'robolectric.pixelCopyRenderMode = hardware' (Robolectric 4.12.2+) in the 'testOptions' block in the 'build.gradle' file. " +
-          "This is advisable to avoid issues with the fidelity of the images.\n" +
-          "Please refer to 'https://github.com/takahirom/roborazzi?tab=readme-ov-file#q-the-images-taken-from-roborazzi-seem-broken' for more information.\n" +
-          "Example:\n$example"
-      )
-    }
-  }
-}
-
-private fun verifyLibraryDependencies(
-  project: Project
-) {
-  val configurations = listOf(
-    // Android
-    "testImplementation",
-    // KMP Android
-    "androidUnitTestImplementation"
-  )
-  fun dependencies(configurations: List<String>): List<Pair<String, List<Pair<String?, String>>>> {
-    return configurations.map { configuration ->
-      try {
-        configuration to project.configurations
-          .getByName(configuration).dependencies
-          .map { dependency -> dependency.group to dependency.name }
-      } catch (e: org.gradle.api.artifacts.UnknownConfigurationException) {
-        configuration to emptyList()
-      }
-    }
-  }
-  verifyLibraryDependencies(dependencies(configurations))
-}
-
-private fun verifyLibraryDependencies(
-  configurationToDependencies: List<Pair<String, List<Pair<String?, String>>>>
-) {
-  val allDependencies = configurationToDependencies.flatMap { it.second }
-  val allConfigurationNames = configurationToDependencies.map { it.first }
-  fun checkExists(libraryName: String) {
-    if (!allDependencies.contains(libraryName.split(":").let { it[0] to it[1] })) {
-      val configurationNames = allConfigurationNames.joinToString(" or ") { "'$it'" }
-      error(
-        "Roborazzi: Please add the following $configurationNames dependency to the 'dependencies' block in the 'build.gradle' file: '$libraryName' for the $configurationNames configuration.\n" +
-          "For your convenience, visit https://www.google.com/search?q=" + URLEncoder.encode(
-          "$libraryName version",
-          "UTF-8"
-        ) + "\n" +
-          "testImplementation(\"$libraryName:version\")"
-      )
-    }
-  }
-
-  val requiredLibraries = listOf(
-    "io.github.takahirom.roborazzi:roborazzi-compose-preview-scanner-support",
-    "junit:junit",
-    "org.robolectric:robolectric",
-    "com.github.sergio-sastre.ComposablePreviewScanner:android",
-  )
-  requiredLibraries.forEach { checkExists(it) }
 }
 
 abstract class GeneratePreviewScreenshotTestsTask : DefaultTask() {
@@ -306,4 +183,128 @@ abstract class GeneratePreviewScreenshotTestsTask : DefaultTask() {
         """.trimIndent()
     )
   }
+}
+
+fun verifyGenerateRobolectricPreviewTests(
+  project: Project,
+  androidExtension: TestedExtension,
+  extension: GenerateComposePreviewRobolectricTestsExtension
+) {
+  if ((extension.enable.orNull) != true) {
+    return
+  }
+  val logger = project.logger
+  project.afterEvaluate {
+    verifyMavenRepository(project)
+    verifyLibraryDependencies(project)
+    verifyAndroidConfig(androidExtension, logger)
+  }
+}
+
+private fun verifyMavenRepository(project: Project) {
+  // Check if the jitpack repository is added.
+  val hasJitpackRepo = project.repositories.any {
+    it is MavenArtifactRepository &&
+      it.url.toString().contains("https://jitpack.io")
+  }
+  // settingsEvaluated{} is not called
+  // when it is already evaluated so we need to call `settingsEvaluated`
+  // before the project is evaluated.
+  // So we need to use the following workaround.
+  // https://github.com/gradle/gradle/issues/27741
+  val setting = (project.gradle as? DefaultGradle)?.getSettings() ?: return
+  val hasJitpackRepoInSettings = setting.pluginManagement.repositories.any {
+    it is MavenArtifactRepository && it.url.toString().contains("https://jitpack.io")
+  } || setting.dependencyResolutionManagement.repositories.any {
+    it is MavenArtifactRepository && it.url.toString().contains("https://jitpack.io")
+  }
+  if (!hasJitpackRepo && !hasJitpackRepoInSettings) {
+    error(
+      "Roborazzi: Please add the following 'maven' repository to the 'repositories' block in the 'build.gradle' file.\n" +
+        "build.gradle: \nrepositories {\nmaven { url 'https://jitpack.io' } \n}\n" +
+        "build.gradle.kts: \nrepositories {\nmaven { url = uri(\"https://jitpack.io\") } \n}\n" +
+        "This is necessary to download the ComposablePreviewScanner."
+    )
+  }
+}
+
+private fun verifyTestConfig(
+  testTaskProvider: TaskCollection<Test>,
+  logger: Logger
+) {
+  testTaskProvider.configureEach { testTask ->
+    if (testTask.systemProperties["roborazzi.record.filePathStrategy"] != "relativePathFromRoborazziContextOutputDirectory") {
+      logger.info(
+        "Roborazzi: Please set 'roborazzi.record.filePathStrategy=relativePathFromRoborazziContextOutputDirectory' in the 'gradle.properties' file. " +
+          "This is advisable to avoid unnecessary path manipulation. " +
+          "Please refer to 'https://github.com/takahirom/roborazzi?tab=readme-ov-file#roborazzirecordfilepathstrategy' for more information."
+      )
+    }
+    if (testTask.systemProperties["robolectric.pixelCopyRenderMode"] != "hardware") {
+      val example = """
+        android {
+          testOptions {
+            unitTests {
+              isIncludeAndroidResources = true
+              all {
+                it.systemProperties["robolectric.pixelCopyRenderMode"] = "hardware"
+              }
+            }
+          }
+        }
+      """.trimIndent()
+      logger.warn(
+        "Roborazzi: Please set 'robolectric.pixelCopyRenderMode = hardware' (Robolectric 4.12.2+) in the 'testOptions' block in the 'build.gradle' file. " +
+          "This is advisable to avoid issues with the fidelity of the images.\n" +
+          "Please refer to 'https://github.com/takahirom/roborazzi?tab=readme-ov-file#q-the-images-taken-from-roborazzi-seem-broken' for more information.\n" +
+          "Example:\n$example"
+      )
+    }
+  }
+}
+
+private fun verifyLibraryDependencies(
+  project: Project
+) {
+  val dependencies: Set<Pair<String?, String>> = project.configurations.flatMap { it.dependencies }
+    .map { it.group to it.name }
+    .toSet()
+  verifyLibraryDependencies(dependencies)
+}
+
+private fun verifyAndroidConfig(androidExtension: TestedExtension, logger: Logger) {
+  if (!androidExtension.testOptions.unitTests.isIncludeAndroidResources) {
+    logger.warn(
+      "Roborazzi: Please set 'android.testOptions.unitTests.isIncludeAndroidResources = true' in the 'build.gradle' file. " +
+        "This is advisable to avoid issues with ActivityNotFoundException."
+    )
+  }
+}
+
+private fun verifyLibraryDependencies(
+  allDependencies: Set<Pair<String?, String>>
+) {
+  fun Set<Pair<String?, String>>.checkExists(libraryName: String) {
+    val dependencies = this
+    val libNameArray = libraryName.split(":")
+    if (!dependencies.contains(libNameArray[0] to libNameArray[1])) {
+      val configurationNames = "'testImplementation'"
+      error(
+        "Roborazzi: Please add the following $configurationNames dependency to the 'dependencies' block in the 'build.gradle' file: '$libraryName' for the $configurationNames configuration.\n" +
+          "For your convenience, visit https://www.google.com/search?q=" + URLEncoder.encode(
+          "$libraryName version",
+          "UTF-8"
+        ) + "\n" +
+          "testImplementation(\"$libraryName:version\")"
+      )
+    }
+  }
+
+  val requiredLibraries = listOf(
+    "io.github.takahirom.roborazzi:roborazzi-compose-preview-scanner-support",
+    "junit:junit",
+    "org.robolectric:robolectric",
+    "com.github.sergio-sastre.ComposablePreviewScanner:android",
+  )
+  requiredLibraries.forEach { allDependencies.checkExists(it) }
 }
