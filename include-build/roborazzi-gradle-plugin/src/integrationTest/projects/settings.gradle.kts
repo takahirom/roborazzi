e808d8758b7346427202ae3caa540683d05b3722
Henry Addo <henry@addhen.org>
2024-10-04 07:38:20 -0700

    Remove snapshot repo as it is not needed anymore

--- include-build/roborazzi-gradle-plugin/src/integrationTest/projects/settings.gradle.kts
+++ include-build/roborazzi-gradle-plugin/src/integrationTest/projects/settings.gradle.kts
@@ -4,9 +4,6 @@ pluginManagement {
     google()
     mavenCentral()
     gradlePluginPortal()
-    maven {
-      url "https://s01.oss.sonatype.org/content/repositories/snapshots/"
-    }
   }
 }
 dependencyResolutionManagement {
@@ -19,9 +16,6 @@ dependencyResolutionManagement {
   repositories {
     google()
     mavenCentral()
-    maven {
-      url "https://s01.oss.sonatype.org/content/repositories/snapshots/"
-    }
   }
 }
 
