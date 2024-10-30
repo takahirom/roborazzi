3d1eb27b9d5347641e9c24ebcdb1f42a2b6eb379
takahirom <takam.dev@gmail.com>
2024-10-30 17:58:31 +0900

    Add roborazzi-ai-openai

---
+++ sample-android/src/test/java/com/github/takahirom/roborazzi/sample/GeminiAiAiTest.kt
@@ -0,0 +1,67 @@
+package com.github.takahirom.roborazzi.sample
+
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.github.takahirom.roborazzi.AiCompareOptions
+import com.github.takahirom.roborazzi.GeminiAiModel
+import com.github.takahirom.roborazzi.ROBORAZZI_DEBUG
+import com.github.takahirom.roborazzi.RobolectricDeviceQualifiers
+import com.github.takahirom.roborazzi.RoborazziOptions
+import com.github.takahirom.roborazzi.RoborazziRule
+import com.github.takahirom.roborazzi.captureRoboImage
+import com.github.takahirom.roborazzi.provideRoborazziContext
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.annotation.Config
+import org.robolectric.annotation.GraphicsMode
+
+@RunWith(AndroidJUnit4::class)
+@GraphicsMode(GraphicsMode.Mode.NATIVE)
+@Config(
+  sdk = [30],
+  qualifiers = RobolectricDeviceQualifiers.NexusOne
+)
+class GeminiAiAiTest {
+  @get:Rule
+  val composeTestRule = createAndroidComposeRule<MainActivity>()
+
+  @get:Rule
+  val roborazziRule = RoborazziRule(
+    options = RoborazziRule.Options(
+      roborazziOptions = RoborazziOptions(
+        compareOptions = RoborazziOptions.CompareOptions(
+          aiCompareOptions = AiCompareOptions(
+            aiModel = GeminiAiModel(
+              apiKey = System.getenv("gemini_api_key") ?: ""
+            ),
+          )
+        )
+      )
+    )
+  )
+
+  @Test
+  fun captureWithAi() {
+    ROBORAZZI_DEBUG = true
+    if (System.getenv("gemini_api_key") == null) {
+      println("Skip the test because gemini_api_key is not set.")
+      return
+    }
+    onView(ViewMatchers.isRoot())
+      .captureRoboImage(
+        roborazziOptions = provideRoborazziContext().options.addedCompareAiAssertions(
+          AiCompareOptions.AiCondition(
+            assertPrompt = "it should have PREVIOUS button",
+            requiredFulfillmentPercent = 90,
+          ),
+          AiCompareOptions.AiCondition(
+            assertPrompt = "it should show First Fragment",
+            requiredFulfillmentPercent = 90,
+          )
+        )
+      )
+  }
+}
