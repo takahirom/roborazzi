045a8250271ebdaa1f63942ad3a746017104f3bb
sanao1006 <naosak1006@gmail.com>
2024-11-04 00:53:45 +0900

    Add settings panel for configuring Roborazzi tasks explanation

--- roborazzi-idea-plugin/src/main/kotlin/com/github/takahirom/roborazzi/idea/settings/AppSettingsComponent.kt
+++ roborazzi-idea-plugin/src/main/kotlin/com/github/takahirom/roborazzi/idea/settings/AppSettingsComponent.kt
@@ -3,26 +3,44 @@ package com.github.takahirom.roborazzi.idea.settings
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBTextField
 import com.intellij.util.ui.FormBuilder
+import javax.swing.Box
+import javax.swing.BoxLayout
 import javax.swing.JComponent
+import javax.swing.JLabel
 import javax.swing.JPanel
+import javax.swing.JSeparator
 
 /**
  * Supports creating and managing a [JPanel] for the Settings Dialog.
  */
 class AppSettingsComponent {
-    private val myMainPanel: JPanel
-    private val imagesPathFromModuleText: JBTextField = JBTextField()
+  private val myMainPanel: JPanel
+  private val imagesPathFromModuleText: JBTextField = JBTextField()
 
-    init {
-        myMainPanel = FormBuilder.createFormBuilder()
-            .addLabeledComponent(JBLabel("Enter images directory path from module: "), imagesPathFromModuleText, 1, false)
-            .addComponentFillVertically(JPanel(), 0)
-            .getPanel()
-    }
+  private val descriptionText = """
+    <html>To enable the display of Roborazzi tasks, please enable<br>
+    <b>Configure all Gradle tasks during Gradle Sync (this can make Gradle Sync slower)</b> in the settings.</html>
+""".trimIndent()
 
+  init {
+    myMainPanel = FormBuilder.createFormBuilder()
+      .addLabeledComponent(
+        JBLabel("Enter images directory path from module: "),
+        imagesPathFromModuleText,
+        1,
+        false
+      )
+      // adjust margin between components
+      .addComponent(JPanel().apply {
+        add(Box.createVerticalStrut(8))
+      })
+      .addComponent(createNoteSection())
+      .addComponent(JBLabel(descriptionText))
+      .addComponentFillVertically(JPanel(), 0)
+      .panel
+  }
     val panel: JPanel
         get() = myMainPanel
-
     val preferredFocusedComponent: JComponent
         get() = imagesPathFromModuleText
 
@@ -31,4 +49,20 @@ class AppSettingsComponent {
         set(newText) {
             imagesPathFromModuleText.setText(newText)
         }
+
+  private fun createNoteSection(): JPanel {
+    val panel = JPanel()
+    panel.layout = BoxLayout(panel, BoxLayout.X_AXIS)
+
+    val label = JLabel("Note")
+    val separator = JSeparator().apply {
+      alignmentY = 0f
+    }
+
+    panel.add(label)
+    panel.add(Box.createHorizontalStrut(8))
+    panel.add(separator)
+
+    return panel
+  }
 }
