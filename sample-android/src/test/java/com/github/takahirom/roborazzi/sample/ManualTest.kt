95ae57cbe570616c6506a067d437d4731f5f43c6
takahirom <takam.dev@gmail.com>
2024-12-12 19:53:36 +0900

    Separate compose lambda test

--- sample-android/src/test/java/com/github/takahirom/roborazzi/sample/ManualTest.kt
+++ sample-android/src/test/java/com/github/takahirom/roborazzi/sample/ManualTest.kt
@@ -1,24 +1,15 @@
 package com.github.takahirom.roborazzi.sample
 
-import android.app.Activity
 import android.graphics.Bitmap
 import android.graphics.Color
 import android.view.View
 import android.widget.TextView
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onParent
 import androidx.compose.ui.test.performClick
-import androidx.compose.ui.unit.dp
 import androidx.core.graphics.applyCanvas
 import androidx.core.graphics.createBitmap
-import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.Espresso.pressBack
 import androidx.test.espresso.action.ViewActions.click
@@ -31,16 +22,11 @@ import com.github.takahirom.roborazzi.Dump
 import com.github.takahirom.roborazzi.ExperimentalRoborazziApi
 import com.github.takahirom.roborazzi.RoboComponent
 import com.github.takahirom.roborazzi.RobolectricDeviceQualifiers
-import com.github.takahirom.roborazzi.RoborazziComposeActivityScenarioOption
-import com.github.takahirom.roborazzi.RoborazziComposeComposableOption
-import com.github.takahirom.roborazzi.RoborazziComposeOptions
 import com.github.takahirom.roborazzi.RoborazziOptions
 import com.github.takahirom.roborazzi.captureRoboAllImage
 import com.github.takahirom.roborazzi.captureRoboGif
 import com.github.takahirom.roborazzi.captureRoboImage
 import com.github.takahirom.roborazzi.captureRoboLastImage
-import com.github.takahirom.roborazzi.fontScale
-import com.github.takahirom.roborazzi.localInspectionMode
 import com.github.takahirom.roborazzi.roboOutputName
 import com.github.takahirom.roborazzi.roborazziSystemPropertyOutputDirectory
 import com.github.takahirom.roborazzi.withComposeTestTag
@@ -160,61 +146,6 @@ class ManualTest {
       text = "Hello View!"
       setTextColor(Color.RED)
     }.captureRoboImage("${roborazziSystemPropertyOutputDirectory()}/manual_view_without_window.png")
-  }
-
-  @OptIn(ExperimentalRoborazziApi::class)
-  @Test
-  fun captureComposeLambdaImage() {
-    captureRoboImage("${roborazziSystemPropertyOutputDirectory()}/manual_compose.png") {
-      Text("Hello Compose!")
-    }
-  }
-
-  @OptIn(ExperimentalRoborazziApi::class)
-  @Test
-  fun captureComposeLambdaImageWithRoborazziComposeOptions() {
-    captureRoboImage(
-      "${roborazziSystemPropertyOutputDirectory()}/manual_compose_with_compose_options.png",
-      roborazziComposeOptions = RoborazziComposeOptions {
-        // We have several options to configure the test environment.
-        fontScale(2f)
-
-        /*
-        We don't specify `localInspectionMode` by default.
-        The default value for `localInspectionMode` in Compose is `false`.
-        This is to maintain higher fidelity in tests.
-        If you encounter issues integrating the library, you can set `localInspectionMode` to `true`.
-
-        localInspectionMode(true)
-         */
-
-        // We can also configure the activity scenario and the composable content.
-        addOption(
-          object : RoborazziComposeComposableOption,
-            RoborazziComposeActivityScenarioOption {
-            override fun configureWithActivityScenario(scenario: ActivityScenario<out Activity>) {
-              scenario.onActivity {
-                it.window.decorView.setBackgroundColor(Color.BLUE)
-              }
-            }
-
-            override fun configureWithComposable(content: @Composable () -> Unit): @Composable () -> Unit {
-              return {
-                Box(Modifier
-                  .padding(10.dp)
-                  .background(color = androidx.compose.ui.graphics.Color.Red)
-                  .padding(10.dp)
-                ) {
-                  content()
-                }
-              }
-            }
-          }
-        )
-      },
-    ) {
-      Text("Hello Compose!")
-    }
   }
 
   @OptIn(ExperimentalRoborazziApi::class)
