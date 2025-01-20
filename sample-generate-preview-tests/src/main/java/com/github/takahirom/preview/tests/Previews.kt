07986a5aa5efab1213a595f7012a146da309d7b2
takahirom <takam.dev@gmail.com>
2025-01-20 09:53:11 +0900

    Add some content for checking PreviewDelayed

--- sample-generate-preview-tests/src/main/java/com/github/takahirom/preview/tests/Previews.kt
+++ sample-generate-preview-tests/src/main/java/com/github/takahirom/preview/tests/Previews.kt
@@ -3,27 +3,9 @@ package com.github.takahirom.preview.tests
 import android.content.res.Configuration
 import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.ElevatedCard
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.lightColorScheme
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
@@ -261,19 +243,29 @@ fun PreviewShowBackgroundWithBackgroundColor() {
 @DelayCaptureRoboImage(delayInMillis = 516L) // 500 ms + 16ms frame
 @Preview
 @Composable
-fun PreviewDelayedVisibilityAnimation() {
+fun PreviewDelayed() {
   var isVisible by remember { mutableStateOf(false) }
+  var counter by remember { mutableStateOf(0) }
 
   // Trigger visibility change with a delay
   LaunchedEffect(Unit) {
     delay(500)
     isVisible = true
   }
+  LaunchedEffect(isVisible) {
+    while(true) {
+      delay(100)
+      counter++
+    }
+  }
 
-  Box(
+  Column(
     modifier = Modifier
       .alpha(if (isVisible) 1f else 0f)
       .size(300.dp)
       .background(Color.Blue)
-  )
+  ) {
+    Text("Counter: ${counter}00ms ")
+    CircularProgressIndicator()
+  }
 }
