4ebc113cf4769796d5fbe9ff4fe88d601609a7f1
takahirom <takam.dev@gmail.com>
2025-04-12 17:30:22 +0900

    Try 4b-it-q4_K_M

--- sample-android/src/test/java/com/github/takahirom/roborazzi/sample/OllamaWithOpenAiApiInterfaceTest.kt
+++ sample-android/src/test/java/com/github/takahirom/roborazzi/sample/OllamaWithOpenAiApiInterfaceTest.kt
@@ -50,7 +50,7 @@ class OllamaWithOpenAiApiInterfaceTest {
               baseUrl = "http://localhost:11434/v1/",
               apiKey = "",
               apiType = OpenAiAiAssertionModel.ApiType.Ollama,
-              modelName = "gemma3:4b-it-q8_0",
+              modelName = "gemma3:4b-it-q4_K_M",
               seed = null,
             ),
           )
