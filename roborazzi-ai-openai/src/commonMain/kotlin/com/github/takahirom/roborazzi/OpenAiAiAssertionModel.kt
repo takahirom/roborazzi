d3e1f5df2f7b03655b55b73347820f6ffccd5cfb
takahirom <takam.dev@gmail.com>
2025-04-12 14:31:26 +0900

    Use 5 minutes of socketTimeoutMillis

--- roborazzi-ai-openai/src/commonMain/kotlin/com/github/takahirom/roborazzi/OpenAiAiAssertionModel.kt
+++ roborazzi-ai-openai/src/commonMain/kotlin/com/github/takahirom/roborazzi/OpenAiAiAssertionModel.kt
@@ -56,7 +56,7 @@ class OpenAiAiAssertionModel(
     }
     install(HttpTimeout) {
       requestTimeoutMillis = INFINITE_TIMEOUT_MS
-      socketTimeoutMillis = 160_000
+      socketTimeoutMillis = 300_000
     }
     if (loggingEnabled) {
       install(Logging) {
