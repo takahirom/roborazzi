a7d507aaa4adb36cf1edda7b49109c01e40e1d17
takahirom <takam.dev@gmail.com>
2025-04-13 14:07:38 +0900

    Fix test

--- sample-android/src/test/java/com/github/takahirom/roborazzi/sample/OllamaWithOpenAiApiInterfaceTest.kt
+++ sample-android/src/test/java/com/github/takahirom/roborazzi/sample/OllamaWithOpenAiApiInterfaceTest.kt
@@ -106,7 +106,7 @@ class OllamaWithOpenAiApiInterfaceTest {
     if (!isOllamaRunning()) {
       // Check if Ollama is running
       println("Ollama is not running. Please start Ollama and try again.")
-      throw AssertionError("Ollama is not running. Please start Ollama and try again.")
+      return
     }
     ROBORAZZI_DEBUG = true
     File(DEFAULT_ROBORAZZI_OUTPUT_DIR_PATH + File.separator + roboOutputName() + ".png").delete()
