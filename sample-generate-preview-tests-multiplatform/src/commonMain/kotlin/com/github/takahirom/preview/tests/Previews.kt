b1e9dc4c0a34e2fac41a1f98650fbf1391728e5f
takahirom <takam.dev@gmail.com>
2025-08-15 19:02:26 +0900

    Upgrade Compose Multiplatform to 1.9.0-beta03 and add Preview parameters support

--- sample-generate-preview-tests-multiplatform/src/commonMain/kotlin/com/github/takahirom/preview/tests/Previews.kt
+++ sample-generate-preview-tests-multiplatform/src/commonMain/kotlin/com/github/takahirom/preview/tests/Previews.kt
@@ -1,7 +1,14 @@
 package com.github.takahirom.preview.tests
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import org.jetbrains.compose.ui.tooling.preview.PreviewParameter
 import org.jetbrains.compose.ui.tooling.preview.PreviewParameterProvider
@@ -20,4 +27,20 @@ fun PreviewNormal() {
 @Composable
 fun PreviewParameter(@PreviewParameter(StringProvider::class) name: String) {
   Text("Multiplatform Preview with PreviewParameter is working, $name!")
+}
+
+@Preview(widthDp = 400, heightDp = 400, showBackground = true, backgroundColor = 0xFF0000FF)
+@Composable
+fun PreviewWithParameters() {
+  Box(
+    modifier = Modifier
+      .size(400.dp)
+      .background(Color(0xFF0000FF)),
+    contentAlignment = Alignment.Center
+  ) {
+    Text(
+      "Preview with custom parameters!",
+      color = Color.White
+    )
+  }
 }
