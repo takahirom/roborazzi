9004decdcd216c098aeec2c68234596734a2adfb
takahirom <takam.dev@gmail.com>
2026-01-17 20:49:58 +0900

    Improve KDoc documentation clarity

--- roborazzi-annotations/src/main/java/com/github/takahirom/roborazzi/annotations/RoboComposePreviewIgnore.kt
+++ roborazzi-annotations/src/main/java/com/github/takahirom/roborazzi/annotations/RoboComposePreviewIgnore.kt
@@ -1,7 +1,7 @@
 package com.github.takahirom.roborazzi.annotations
 
 /**
- * Annotation for excluding a @Preview composable function from Roborazzi's preview screenshot tests.
+ * Annotation for excluding a @Preview composable function from Roborazzi's Compose Preview Support.
  *
  * When a @Preview function is annotated with @RoboComposePreviewIgnore, it will be excluded
  * from the generated preview screenshot tests.
@@ -15,8 +15,6 @@ package com.github.takahirom.roborazzi.annotations
  *   // This preview will be excluded from screenshot tests
  * }
  * ```
- *
- * To use this annotation, you must include the roborazzi-compose-preview-scanner-support library.
  *
  * Note: If you use a custom tester class that overrides the scanner configuration without calling
  * `excludeIfAnnotatedWithAnyOf(RoboComposePreviewIgnore::class.java)`, this annotation will have no effect.
