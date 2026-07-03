b583801333382aa480a01e369bda74ec75983227
Takahiro Menju <takam.dev@gmail.com>
2026-07-04 01:09:55 +0900

    Implement iOS ImageIoFormat to unblock common pipeline

--- include-build/roborazzi-core/src/iosMain/kotlin/com/github/takahirom/roborazzi/ImageIoFormat.ios.kt
+++ include-build/roborazzi-core/src/iosMain/kotlin/com/github/takahirom/roborazzi/ImageIoFormat.ios.kt
@@ -1,10 +1,21 @@
 package com.github.takahirom.roborazzi
 
+/**
+ * Marker [ImageIoFormat] for iOS. The iOS canvas ([UIImageRoboCanvas]) always
+ * writes PNG via `UIImagePNGRepresentation` and does not consult this value, so
+ * the default format is a simple singleton.
+ */
+@ExperimentalRoborazziApi
+object IosImageIoFormat : ImageIoFormat
+
+@ExperimentalRoborazziApi
 @Suppress("FunctionName")
 actual fun LosslessWebPImageIoFormat(): ImageIoFormat {
-  TODO("NOT IMPLEMENTED YET")
+  // WebP encoding is not implemented for iOS yet.
+  TODO("Lossless WebP output is not supported on iOS")
 }
 
+@ExperimentalRoborazziApi
 actual fun ImageIoFormat(): ImageIoFormat {
-  TODO("NOT IMPLEMENTED YET")
+  return IosImageIoFormat
 }
